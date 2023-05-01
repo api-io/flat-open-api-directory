de7098a5906b171ad658131a9a62be078b5fcc7e
dinavinter <dina.vinter@sap.com>
2023-05-01 04:20:46 +0300

    ss

--- .github/flat/installation.ts
+++ .github/flat/installation.ts
@@ -1,17 +1,18 @@
 import {readJSON, writeJSON} from 'https://deno.land/x/flat/mod.ts';
+import {exists} from "https://deno.land/std@0.170.0/fs/mod.ts";
 
 const downloaded_filename = Deno.args[0];
 const meta = JSON.parse(Deno.env.get("META"));
 
-//merge repositories and installation meta
-const repositories = await readJSON(downloaded_filename);
-const installation = {...meta, ...repositories};
+//write repositories and installation meta
+const installation = {...meta, ...await readJSON(downloaded_filename)};
 const {id, store: {index}} = meta;
 writeJSON(index, installation);
 
 //store in global index
 const global_index = await getIndexPath();
-writeJSON(global_index, {...await readJSON(global_index), [id]: installation});
+const global_index_content = await exists(global_index) ? await readJSON(global_index) : {};
+writeJSON(global_index, {...global_index_content, [id]: installation});
 
 
 async function getIndexPath() {
