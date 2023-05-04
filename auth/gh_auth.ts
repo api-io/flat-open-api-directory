14e3702d7abe69433f83d0106557f2e908e3c551
dinavinter <dina.vinter@sap.com>
2023-05-04 03:14:43 +0300

    sdd

--- auth/gh_auth.ts
+++ auth/gh_auth.ts
@@ -1,10 +1,19 @@
 import { createAppAuth } from "https://cdn.pika.dev/@octokit/auth-app?dts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
+import {readTXT} from "https://deno.land/x/flat@0.0.15/src/txt.ts";
+
+
+// deno run --allow-read --allow-write --allow-env  auth/gh_auth.ts > jwt.txt
+// authorization=`cat jwt.txt`
+// id="33351991"
+// gh api -X POST -H "authorization=bearer $authorization" /app/installations/$id/access_tokens --jq -r .token
+//echo "token=$(gh api -X POST -H "authorization=bearer $authorization" /app/installations/${{env.id}}/access_tokens --jq -r .token)" 
 
 const env=config();
+const pm= await readTXT(env.PRIVATE_KEY_FILE);
 const auth = createAppAuth({
     appId: env.APP_ID,
-    privateKey: env.PRIVATE_KEY,
+    privateKey: pm,
     clientId: env.CLIENT_ID,
     clientSecret: env.CLIENT_SECRET
   });
